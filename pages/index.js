e6dc24e4e90fecfe19d0a74f905a59fe741c7b8d
jon-cron <jonathanacm95@gmail.com>
2023-06-26 13:32:25 -0600

    next is neat

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
+// NOTE the next router works differently. By default the first index within the pages folder will be your domain www.domain.com/, then the next folder will be a new screen with its own index
 export default function Home() {
   return (
     <div>
